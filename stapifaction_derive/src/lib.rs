f54b47b60f1945d74cb80ae58973cf651e194ff7
leruaa <aurelien.catinon@gmail.com>
2023-11-27 15:39:41 +0000

    Handle Serde fields attibutes skip and rename

--- stapifaction_derive/src/lib.rs
+++ stapifaction_derive/src/lib.rs
@@ -1,18 +1,21 @@
-use darling::FromDeriveInput;
+use persistable_input_receiver::expand_derive_persistable;
 use proc_macro::TokenStream;
-use quote::quote;
+use serde_derive_internals::{ast::Container, Ctxt, Derive};
 use syn::{parse_macro_input, DeriveInput};
-
-use crate::persistable_input_receiver::PersistableInputReceiver;
 
 mod persistable_input_receiver;
 
-#[proc_macro_derive(Persistable, attributes(persistable))]
+#[proc_macro_derive(Persistable, attributes(persistable, serde))]
 pub fn persistable(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let receiver = PersistableInputReceiver::from_derive_input(&input).unwrap();
 
-    let expanded = quote! { #receiver };
+    let cx = Ctxt::new();
+    let serde_container = Container::from_ast(&cx, &input, Derive::Serialize)
+        .expect("Failed to create Serde container");
+
+    let expanded = expand_derive_persistable(serde_container);
+
+    cx.check().unwrap();
 
     TokenStream::from(expanded)
 }
--- stapifaction_derive/src/persistable_input_receiver.rs
+++ stapifaction_derive/src/persistable_input_receiver.rs
@@ -1,90 +1,103 @@
-use darling::{ast::Data, util::Ignored, FromDeriveInput, FromField};
+use darling::{FromDeriveInput, FromField};
 use itertools::Itertools;
 use proc_macro2::TokenStream;
-use quote::{format_ident, quote, ToTokens};
-use syn::{Ident, Type};
+use quote::{format_ident, quote};
+use serde_derive_internals::ast::{Container, Data};
+use syn::{Ident, Member, Type};
+
+pub fn expand_derive_persistable(serde_contrainer: Container) -> TokenStream {
+    let Container {
+        ident,
+        data,
+        original,
+        ..
+    } = serde_contrainer;
+    let PersistableInputReceiver { path } =
+        PersistableInputReceiver::from_derive_input(original).unwrap();
+
+    let ident_str = ident.to_string();
+    let container_ident = format_ident!("{}Container", ident);
+
+    if let Data::Struct(_, fields) = data {
+        let fields = fields
+            .into_iter()
+            .map(|serde_field| {
+                PersistableField::from_field(serde_field.original).map(|f| (serde_field, f))
+            })
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap();
+
+        let (id, _) = fields
+            .iter()
+            .find(|(_, f)| f.id)
+            .expect("An id must be specified");
+
+        let id_ident = &id.member;
+        let fields_count = fields.len();
+        let (field_idents_str, field_idents) = fields
+            .iter()
+            .filter(|(f, _)| !f.attrs.skip_serializing())
+            .filter_map(|(f, _)| match &f.member {
+                Member::Named(ident) => Some((f.attrs.name().serialize_name(), ident)),
+                Member::Unnamed(_) => None,
+            })
+            .multiunzip::<(Vec<_>, Vec<_>)>();
+
+        quote! {
+            struct #container_ident<'a> {
+                entity: &'a #ident,
+            }
+
+            impl<'a> stapifaction::serde::Serialize for #container_ident<'a> {
+                fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+                where
+                    S: stapifaction::serde::Serializer,
+                {
+                    let mut state = serializer.serialize_struct(#ident_str, #fields_count)?;
+
+                    #( stapifaction::serde::SerializeStruct::serialize_field(&mut state, #field_idents_str, &self.entity.#field_idents)?; )*
+
+                    stapifaction::serde::SerializeStruct::end(state)
+                }
+            }
+
+            impl stapifaction::Persistable for #ident {
+                fn path() -> std::path::PathBuf {
+                    #path.parse().unwrap()
+                }
+
+                fn subsets<'a>(
+                    &'a self,
+                ) -> std::collections::HashMap<std::path::PathBuf, Box<dyn stapifaction::serde::ErasedSerialize + 'a>>
+                {
+                    let container = #container_ident { entity: self };
+                    std::collections::HashMap::from([(
+                        format!("{}", self.#id_ident).into(),
+                        Box::new(container) as Box<dyn stapifaction::serde::ErasedSerialize>,
+                    )])
+                }
+            }
+        }
+    } else {
+        unimplemented!("Enums aren't supported");
+    }
+}
 
 #[derive(Debug, FromDeriveInput)]
 #[darling(attributes(persistable), supports(struct_any))]
 pub struct PersistableInputReceiver {
-    pub ident: Ident,
     pub path: String,
-    pub data: Data<Ignored, Field>,
-}
-
-impl ToTokens for PersistableInputReceiver {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        let PersistableInputReceiver {
-            ref ident,
-            ref data,
-            path,
-        } = self;
-
-        let ident_str = ident.to_string();
-        let container_ident = format_ident!("{}Container", ident);
-
-        if let Data::Struct(data) = data {
-            let id = data
-                .fields
-                .iter()
-                .find(|f| f.id)
-                .expect("An id must be specified");
-            let id_ident = id.ident.as_ref().expect("The id must be have an ident");
-            let fields_count = data.len();
-            let (field_idents_str, field_idents) = data
-                .iter()
-                .filter_map(|f| f.ident.as_ref())
-                .map(|i| (i.to_string(), i))
-                .multiunzip::<(Vec<_>, Vec<_>)>();
-
-            tokens.extend(quote! {
-                struct #container_ident<'a> {
-                    entity: &'a #ident,
-                }
-
-                impl<'a> stapifaction::serde::Serialize for #container_ident<'a> {
-                    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-                    where
-                        S: stapifaction::serde::Serializer,
-                    {
-                        let mut state = serializer.serialize_struct(#ident_str, #fields_count)?;
-
-                        #( stapifaction::serde::SerializeStruct::serialize_field(&mut state, #field_idents_str, &self.entity.#field_idents)?; )*
-
-                        stapifaction::serde::SerializeStruct::end(state)
-                    }
-                }
-
-                impl stapifaction::Persistable for #ident {
-                    fn path() -> std::path::PathBuf {
-                        #path.parse().unwrap()
-                    }
-
-                    fn subsets<'a>(
-                        &'a self,
-                    ) -> std::collections::HashMap<std::path::PathBuf, Box<dyn stapifaction::serde::ErasedSerialize + 'a>>
-                    {
-                        let container = #container_ident { entity: self };
-                        std::collections::HashMap::from([(
-                            format!("{}", self.#id_ident).into(),
-                            Box::new(container) as Box<dyn stapifaction::serde::ErasedSerialize>,
-                        )])
-                    }
-                }
-            })
-        } else {
-            unimplemented!("Enums aren't supported");
-        }
-    }
 }
 
 #[derive(Debug, FromField)]
 #[darling(attributes(persistable))]
-pub struct Field {
+pub struct PersistableField {
     pub ident: Option<Ident>,
     pub ty: Type,
     #[darling(default)]
     pub id: bool,
+    #[darling(default)]
+    pub subset: bool,
 }
 
 #[cfg(test)]
@@ -109,16 +122,6 @@ mod tests {
 
         let receiver = PersistableInputReceiver::from_derive_input(&di).unwrap();
 
-        assert_eq!(
-            receiver
-                .data
-                .take_struct()
-                .unwrap()
-                .iter()
-                .filter(|f| f.id)
-                .count(),
-            1
-        );
         assert_eq!(receiver.path, "users");
     }
 }
