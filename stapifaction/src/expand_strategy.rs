a3b67a99e165783950aad3204a5e215520a6574c
leruaa <aurelien.catinon@gmail.com>
2023-12-19 18:47:34 +0000

    Allow to customize expand strategies

--- stapifaction/src/expand_strategy.rs
+++ stapifaction/src/expand_strategy.rs
@@ -1,5 +1,26 @@
+use std::path::{Path, PathBuf};
+
 pub enum ExpandStrategy {
-    SubsetsInSeparateFolders,
-    SubsetsGroupedInTheSameFolder,
+    SubsetsInSeparateFolders(String),
+    SubsetsGroupedInUniqueFolder(String),
     IdAsFileName,
+}
+
+impl ExpandStrategy {
+    pub fn resolve_path(&self, parent_path: &Path, entity_name: Option<PathBuf>) -> PathBuf {
+        let mut path = parent_path.to_path_buf(); //.join();
+
+        match self {
+            ExpandStrategy::SubsetsInSeparateFolders(name) => {
+                path.push(entity_name.unwrap_or_default());
+                path.push(name);
+            }
+            ExpandStrategy::SubsetsGroupedInUniqueFolder(name) => {
+                path.push(entity_name.map(|e| e.join(name)).unwrap_or_default());
+            }
+            ExpandStrategy::IdAsFileName => {}
+        }
+
+        path
+    }
 }
