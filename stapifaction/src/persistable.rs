a07422c45ae86e7930504dba9ad1d97fcbd05f4b
leruaa <aurelien.catinon@gmail.com>
2024-04-01 18:08:06 +0000

    Add docs

--- stapifaction/src/persistable.rs
+++ stapifaction/src/persistable.rs
@@ -4,24 +4,34 @@ use erased_serde::Serialize as ErasedSerialize;
 
 use crate::{ExpandStrategy, ResolvablePath};
 
+/// Persistable defines how to persist a given entity.
 pub trait Persistable {
+    /// The path where the entity will be persisted.
     fn path(&self) -> ResolvablePath;
+    /// The strategy used to expand childrens.
     fn expand_strategy(&self) -> Option<ExpandStrategy>;
+    /// The entity that will be serialized.
     fn serializable_entity<'e>(&'e self) -> Option<Box<dyn ErasedSerialize + 'e>>;
+    /// The entity children.
     fn children<'e>(&'e self) -> Box<dyn Iterator<Item = (PathBuf, Cow<'e, Child<'e>>)> + 'e>;
 }
 
+/// A child.
 #[derive(Clone)]
 pub enum Child<'a> {
+    /// A subset child.
     Subset(&'a dyn Persistable),
+    /// A children collections.
     Collection(Box<[Child<'a>]>),
 }
 
 impl<'a> Child<'a> {
+    /// Creates a new subset.
     pub fn subset<P: Persistable>(subset: &'a P) -> Self {
         Self::Subset(subset)
     }
 
+    /// Creates a new collection.
     pub fn collection<I, P>(collection: I) -> Self
     where
         I: Iterator<Item = &'a P> + 'a,
