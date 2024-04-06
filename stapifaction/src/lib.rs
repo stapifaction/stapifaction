85659ea1dca6c8858d000237569c883643615898
leruaa <aurelien.catinon@gmail.com>
2024-04-06 15:21:34 +0000

    Don't impl Serialize in macro

--- stapifaction/src/lib.rs
+++ stapifaction/src/lib.rs
@@ -23,5 +23,4 @@ pub use stapifaction_derive::Persistable;
 
 pub mod serde {
     pub use erased_serde::Serialize as ErasedSerialize;
-    pub use serde::{ser::SerializeStruct, Serialize, Serializer};
 }
